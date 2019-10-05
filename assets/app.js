285c102b2609d5e275dbf9eb222f099b45e3af44
Kimberlyn Creary <kimocreary@gmail.com>
2019-10-05 14:15:38 -0400

    firebase data referenced

--- assets/app.js
+++ assets/app.js
@@ -9,11 +9,27 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var trainData = firebase.database()
+// var connectionsRef = trainData.ref("/connections");
 trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
     console.log(childSnapshot.val());
+    var trainsName = childSnapshot.val().name;
+    var trainsDestination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+        console.log("trainsName");
+        console.log("trainsDestination");
+        console.log("firstTrainTime");
+        console.log("trainFrequency");
+
+    var firstTrainTimePretty = moment.unix(firstTrainTime).format("HH.mm")
+    var trainFrequency = moment.unix(trainFrequency).format("mm")
+
+
     // Loop through the for loop 
     // Add to the html table
     }) 
+
+
 $("#submitbutton").on("click", function(event) {
     event.preventDefault()
     console.log("submit button clicked")
